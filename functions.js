b338ef89baa4022586a30bc4964f0427012a666b
Nomche Jovanoski <cuculj.989@gmail.com>
2023-04-04 04:42:48 +0200

    BUllshits

--- functions.js
+++ functions.js
@@ -1,3 +1,22 @@
 async function fetchData(string){
     console.log(string);
+    var test={
+        title:"test",
+        contnet:"test",
+        author:"test",
+        img:"test",
+        date:"test"    }
+    document.getElementsByTagName('section')[0].append(createCard(test));
+}
+function createCard(apiData){
+    let title=document.createElement('h2').innerText=apiData.title,
+        contnet=document.createElement('p').innerText=apiData.contnet,
+        author=document.createElement('h3').innerText=apiData.author,
+        img=document.createElement('img').innerText=apiData.img,
+        date=document.createElement('h3').innerText=apiData.date;
+
+      let card=document.createElement('div');
+      card.append(title,contnet,author);
+      return card;
+
 }
