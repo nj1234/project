78bc5c49651d3a01c00317ce7296215618bb71f9
Nomche Jovanoski <cuculj.989@gmail.com>
2023-04-04 15:56:12 +0200

    test

--- functions.js
+++ functions.js
@@ -9,12 +9,16 @@ async function fetchData(string){
     document.getElementsByTagName('section')[0].append(createCard(test));
 }
 function createCard(apiData){
-    let title=document.createElement('h2').innerText=apiData.title,
-        contnet=document.createElement('p').innerText=apiData.contnet,
-        author=document.createElement('h3').innerText=apiData.author,
-        img=document.createElement('img').innerText=apiData.img,
-        date=document.createElement('h3').innerText=apiData.date;
-
+    let title=document.createElement('h2'),
+        contnet=document.createElement('p'),
+        author=document.createElement('h3'),
+        img=document.createElement('img'),
+        date=document.createElement('h3');
+        title.innerText=apiData.title;
+        contnet.innerText=apiData.contnet;
+        author.innerText=apiData.author;
+        img.innerText=apiData.img;
+        date.innerText=apiData.date;
       let card=document.createElement('div');
       card.append(title,contnet,author);
       return card;
